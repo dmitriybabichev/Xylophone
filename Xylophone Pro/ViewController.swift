f59963932a83965902e2f000aa03a115b1c42c46
Бабичев Дмитрий Александрович <babichevda@sibintek.ru>
2023-01-29 21:17:17 +0300

    Initial commit

--- Xylophone Pro/ViewController.swift
+++ Xylophone Pro/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var player: AVAudioPlayer!
+    let keyTitles = ["C", "D", "E", "F", "G", "A", "B"]
+
+    @IBOutlet var keys: [UIButton]!
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.1, delay: 0, options: .autoreverse) {
+            sender.alpha = 0.5
+        } completion: { _ in sender.alpha = 1 }
+
+        guard let currentTitle = sender.currentTitle else { return }
+        playSound(note: currentTitle)
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
 
+    func configureUI() {
+        for key in keys {
+            key.layer.cornerRadius = 10
+        }
+    }
+
+    func playSound(note: String) {
+        guard let url = Bundle.main.url(forResource: note, withExtension: "wav") else { return }
+        player = try! AVAudioPlayer(contentsOf: url)
+        player.play()
+    }
 }
 
